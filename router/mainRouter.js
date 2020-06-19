09bf3a683453cab80a8dbe192a5879578d49aa2e
vapvin <tpehdj12@daum.net>
2020-06-19 22:41:08 +0900

    Router Clear

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -2,4 +2,9 @@ import express from "express";
 import routes from "../routes";
 const mainRouter = express();
 
+mainRouter.get(routes.main, (req, res) => res.send("main"));
+mainRouter.get(routes.apiDocumentation, (req, res) => {
+  res.send("apiDocumentation");
+});
+
 export default mainRouter;
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,4 +2,8 @@ import express from "express";
 import routes from "../routes";
 const userRouter = express();
 
+userRouter.get(routes.login, (req, res) => res.send("login"));
+userRouter.get(routes.join, (req, res) => res.send("join"));
+userRouter.get(routes.confirmAccount, (req, res) => res.send("confirmAccount"));
+
 export default userRouter;
